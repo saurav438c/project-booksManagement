9ce882b55f5124e43be2961212c1ba0164c6b3b7
funtionup-aug <arungadhe11@gmail.com>
2022-09-20 12:54:26 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const route = require('./route/routes.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://sonupk:1HivF6DXHWanVcYu@cluster0.vtjazgb.mongodb.net/group20Database", {
     useNewUrlParser: true
